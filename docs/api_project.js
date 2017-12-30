09e73c2aef49a5fbf468d27ff4c8213dd80c5a56
Menasi Ephrem <menasiephrem@gmail.com>
2017-12-30 18:37:56 +0300

    final touch

--- docs/api_project.js
+++ docs/api_project.js
@@ -1,14 +1,14 @@
 define({
   "name": "Gebeya Dating Api",
   "version": "1.0.0",
-  "description": "Gebeya Dating API",
-  "url": "https://api.dating.com",
+  "description": "A simple dation api for Gebeya ",
+  "url": "https://gebeya-date.herokuapp.com",
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2017-12-30T15:07:13.845Z",
+    "time": "2017-12-30T15:36:35.946Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
